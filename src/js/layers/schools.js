fc573c8c1699aae1669e26000613489888da1731
Elif Koc <elifkoc@nyu.edu>
2016-06-30 13:21:23 -0700

    Added mitigation to legend

--- src/js/layers/schools.js
+++ src/js/layers/schools.js
@@ -54,27 +54,32 @@ module.exports = {
           });
           
           group.buildings.forEach(function(b) {
-            if (b.mitigationType !== "") {
-              if (!mitig[b.mitigationType]) mitig[b.mitigationType] = 0;
-              mitig[b.mitigationType]++;
+            if (b.mitigation === "Yes") {
+              if (!mitig[b.mitigation])
+                mitig[b.mitigation] = 0;
+              mitig[b.mitigation]++;
             }
         })
           
+          //Calculates how many buildings within a particular school have a risk of X or mitigation. 
           var getRisk = r => risks[r] || 0;
-          group.mitigation = mitig;
+          var getMitig = m => mitig[m] || 0;
+
+          group.mitigation = getMitig("Yes");
           group.moderateHazards = getRisk('Moderate') + getRisk('Moderate to High');
           group.highHazards = getRisk('High') + getRisk('High to Very High') + getRisk('Very High');
-          group.hazards = group.moderateHazards + group.highHazards + getRisk('Low') + getRisk('Low to Moderate');
+          group.hazards = group.moderateHazards + group.highHazards + getRisk('Low') + getRisk('Low to Moderate') + getRisk('Missing Data');
 
           if (!grouped[k].coords) {
             console.log(`No coordinates for ${k}`);
             continue;
           }
           
+          //Percentage of buildings with moderate/high risk
           var modRisk = (group.moderateHazards / group.hazards );
           var highRisk = (group.highHazards / group.hazards );
-          console.log(modRisk);
-
+          
+          //Assigns a color to each school based on risk
           function riskColor() {
             if (highRisk > .25) {
               return "high";
@@ -82,11 +87,21 @@ module.exports = {
             else if (modRisk > .3) {
               return "medium";
             }
+        };          
+          
+          //Assigns a color to each school based on risk
+          function isMitigated() {
+            if (group.mitigation > 0) {
+              return "mitigated";
+            }
+            else {
+              return "";
+            }
         };
-         
+         console.log(isMitigated());
           var marker = leaflet.marker(grouped[k].coords, {
             icon: leaflet.divIcon({
-              className: "school-marker " + riskColor()
+              className: "school-marker " + riskColor() + " " + isMitigated()
             }),
           });
           marker.bindPopup(template(grouped[k]), { className: "school-popup", maxHeight: 300 });
@@ -104,13 +119,18 @@ module.exports = {
   viewbox: [[45.5948097, -124.387099], [48.5353, -120.29922]],
   key: `
       <h2>Earthquake Preparedness in Washington</h2>
+      <ul class="top-key">
+        <li>
+          <i class="school-marker shape square"></i> Completed seismic upgrade</li>
+        <li>
+          <i class="school-marker shape"></i> No seismic upgrade</li>
+      </ul>
       <ul>
-
         <li> <i class="school-marker high"></i> Very high to high damage likely for > 25% of buildings
         <li> <i class="school-marker medium"></i> Moderate damage likely for > 30% of buildings
         <li> <i class="school-marker"></i> 
             Low to moderate risk of damage
       </ul>
-<p class="chatter"> School audit results from a random sampling of districts. Earthquake risk takes several factors into consideration, including building type, potential for ground shaking, and soil type. Some schools have mitigated their risk of damage with facility upgrades. To see which schools have done so, click on each marker to expand details.</p>
+<p class="chatter">Earthquake risk includes building type, potential for ground shaking, and soil type. Some schools have mitigated their risk of damage with facility upgrades.</p>
 <div class="source">Source: <a href="http://www.k12.wa.us/">OSPI</a></div> `
 }
