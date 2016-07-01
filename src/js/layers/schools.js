8a0a53e7845656b817da451144b15f00b3733fb5
Elif Koc <elifkoc@nyu.edu>
2016-07-01 11:43:38 -0700

    Cleaned up

--- src/js/layers/schools.js
+++ src/js/layers/schools.js
@@ -24,7 +24,7 @@ module.exports = {
         var grouped = {};
         schools.forEach(function(row) {
           var key = row.district.trim() + "|" + row.school.trim();
-          
+
           if (!grouped[key]) grouped[key] = {
             coords: gps[key],
             buildings: [],
@@ -34,33 +34,34 @@ module.exports = {
           grouped[key].buildings.push(row);
 
         });
-        
+
         var layer = leaflet.layerGroup();
         for (var k in grouped) {
+          
           var group = grouped[k];
+          if (!grouped[k].coords) {
+            console.log(`No coordinates for ${k}`);
+            continue;
+          };
           
           var risks = {};
-          var mitig = {};
+          var mitig = {};   
           var years = group.buildings.map(b => b.built);
+          
           group.newest = Math.max.apply(null, years);
           group.oldest = Math.min.apply(null, years);
           if (group.oldest === 0) {
             group.oldest = 1900;
-          }
+          };
+          
           group.buildings.forEach(function(b) {
             if (!risks[b.hazard]) risks[b.hazard] = 0;
             risks[b.hazard]++;
             
-          });
-          
-          group.buildings.forEach(function(b) {
-            if (b.mitigation === "Yes") {
-              if (!mitig[b.mitigation])
-                mitig[b.mitigation] = 0;
+            if (!mitig[b.mitigation]) mitig[b.mitigation] = 0;
               mitig[b.mitigation]++;
-            }
-        })
-          
+          });
+
           //Calculates how many buildings within a particular school have a risk of X or mitigation. 
           var getRisk = r => risks[r] || 0;
           var getMitig = m => mitig[m] || 0;
@@ -70,15 +71,10 @@ module.exports = {
           group.highHazards = getRisk('High') + getRisk('High to Very High') + getRisk('Very High');
           group.hazards = group.moderateHazards + group.highHazards + getRisk('Low') + getRisk('Low to Moderate') + getRisk('Missing Data');
 
-          if (!grouped[k].coords) {
-            console.log(`No coordinates for ${k}`);
-            continue;
-          }
-          
           //Percentage of buildings with moderate/high risk
           var modRisk = (group.moderateHazards / group.hazards );
           var highRisk = (group.highHazards / group.hazards );
-          
+
           //Assigns a color to each school based on risk
           function riskColor() {
             if (highRisk > .25) {
@@ -87,8 +83,8 @@ module.exports = {
             else if (modRisk > .25) {
               return "medium";
             }
-        };          
-          
+          };          
+
           var marker = leaflet.marker(grouped[k].coords, {
             icon: leaflet.divIcon({
               className: "school-marker " + riskColor()
@@ -107,13 +103,12 @@ module.exports = {
 
   },
   viewbox: [[45.5948097, -124.387099], [48.5353, -120.29922]],
-  key: `
-      <h2>Earthquake Preparedness in Washington</h2>
-      <ul>
-        <li> <i class="school-marker high"></i> Very high to high damage likely for > 25% of buildings
-        <li> <i class="school-marker medium"></i> Moderate damage likely for > 25% of buildings
-        
-      </ul>
+  key: `<h2>Earthquake Preparedness in Washington</h2>
+<ul>
+<li> <i class="school-marker high"></i> Very high to high damage likely for > 25% of buildings
+<li> <i class="school-marker medium"></i> Moderate damage likely for > 25% of buildings
+
+</ul>
 <p class="chatter">Earthquake risk includes building type, potential for ground shaking, and soil type. Some schools have mitigated their risk of damage with facility upgrades.</p>
 <div class="source">Source: <a href="http://www.k12.wa.us/">OSPI</a></div> `
 }
